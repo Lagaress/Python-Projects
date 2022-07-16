c240bf2adcf2b5b236b4e0970ebaa197c4f6ea07
Jesús Lagares <48330849+Lagaress@users.noreply.github.com>
2022-07-16 15:50:04 +0200

    Create csv if doesnt exist special case added

--- Scripting/Summit/fill.py
+++ Scripting/Summit/fill.py
@@ -1,6 +1,7 @@
 import csv
 import names 
 import random
+import os.path
 
 def get_random_name():
     return names.get_full_name()
@@ -22,3 +23,7 @@ def create_csv():
             vector_random_number = get_vector4_random_number()
             writer.writerow([get_random_name(),vector_random_number[0],vector_random_number[1],vector_random_number[2],vector_random_number[3]])
 
+def create_csv_if_not_exist():
+    if (not os.path.exists('data.csv')):
+        create_csv()
+    
--- Scripting/Summit/main.py
+++ Scripting/Summit/main.py
@@ -2,6 +2,7 @@ import fill
 import distribute
 
 def main():
+    fill.create_csv_if_not_exist()
     distribute.distribute_people_in_companies()
 
 if __name__=="__main__":
